2495b5b29aa41ee93ab0c31914e174fcf72f6e15
c3k4ah <dominickp20.aps1b@gmail.com>
2021-12-26 01:55:33 +0300

    chat Room + chat 100%

--- lib/views/chat_room.dart
+++ lib/views/chat_room.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get_core/get_core.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:hermes/colors.dart';
-import 'package:hermes/views/widget/navBar.dart';
+
+// ignore: unused_import
+import 'package:hermes/views/widget/sendMessage.dart';
+import 'package:line_icons/line_icons.dart';
 
 class CHatRoom extends StatefulWidget {
   const CHatRoom({Key? key}) : super(key: key);
@@ -15,61 +18,121 @@ class _CHatRoomState extends State<CHatRoom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Appbar(),
-      body: Container(
-        width: Get.width,
-        height: Get.height * .9,
-        color: dark,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(top: 20, right: 15),
-                      width: Get.width * .55,
-                      padding: EdgeInsets.all(18),
-                      decoration: BoxDecoration(
-                        color: darkSecond,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(45),
-                          bottomLeft: Radius.circular(45),
-                          topRight: Radius.circular(35),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text("Hello !",
-                            style: TextStyle(color: white, fontSize: 20)),
-                      )),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                      margin: EdgeInsets.only(top: 20, left: 15),
-                      width: Get.width * .55,
-                      padding: EdgeInsets.all(18),
-                      decoration: BoxDecoration(
-                        color: darkSecond,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(35),
-                          bottomRight: Radius.circular(45),
-                          topRight: Radius.circular(45),
-                        ),
-                      ),
-                      child: Center(
-                        child: Expanded(
-                          child: Text(
-                              "Salut! Je suis Anne, je suis interéssé pas votre profile ",
-                              style: TextStyle(color: white, fontSize: 20)),
-                        ),
-                      )),
-                ],
-              ),
-            ],
+      appBar: AppBar(
+        backgroundColor: dark,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            LineIcons.angleLeft,
+            color: white,
           ),
+        ),
+        actions: <Widget>[
+          Container(
+              width: Get.width * .7,
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: redBlood,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: Get.width * .45,
+                    child: Text("Dominick Randriamanantena Grégoire",
+                        style: TextStyle(
+                            color: white,
+                            fontSize: 20,
+                            overflow: TextOverflow.ellipsis)),
+                  ),
+                ],
+              )),
+          PopupMenuButton(
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(child: Text("Marquer comme non lu")),
+                PopupMenuItem(
+                  child: Text("Voir profile"),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Message(),
+            Container(alignment: Alignment.bottomCenter, child: Send())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Message extends StatelessWidget {
+  const Message({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      width: Get.width,
+      height: Get.height * .9,
+      color: dark,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 20, right: 15),
+                    width: Get.width * .55,
+                    padding: EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      color: darkSecond,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(45),
+                        bottomLeft: Radius.circular(45),
+                        topRight: Radius.circular(35),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text("Hello !",
+                          style: TextStyle(color: white, fontSize: 20)),
+                    )),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 20, left: 15),
+                    width: Get.width * .55,
+                    padding: EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      color: darkSecond,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35),
+                        bottomRight: Radius.circular(45),
+                        topRight: Radius.circular(45),
+                      ),
+                    ),
+                    child: Center(
+                      child: Expanded(
+                        child: Text(
+                            "Salut! Je suis Anne, je suis interéssé pas votre profile ",
+                            style: TextStyle(color: white, fontSize: 20)),
+                      ),
+                    )),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/views/widget/sendMessage.dart
+++ lib/views/widget/sendMessage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hermes/colors.dart';
+import 'package:line_icons/line_icons.dart';
 
 class Send extends StatelessWidget implements PreferredSizeWidget {
   const Send({Key? key}) : super(key: key);
@@ -7,7 +10,62 @@ class Send extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    
+      height: 60,
+      color: dark,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                LineIcons.paperclip,
+                color: redBlood,
+              )),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 7),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            //height: 50,
+            width: Get.width * .7,
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                  width: 2,
+                  color: redBlood,
+                )),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {},
+                    style: TextStyle(color: white),
+                    decoration: InputDecoration(
+                      hintText: "message",
+                      hintStyle: TextStyle(
+                        color: white.withOpacity(.5),
+                      ),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MaterialButton(
+              height: 40,
+              minWidth: 15,
+              color: redBlood,
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25)),
+              child: Icon(
+                LineIcons.paperPlane,
+                color: white,
+              )),
+        ],
+      ),
     );
   }
 }
